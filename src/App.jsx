064106d0a9b83ccf9e565f71b590b3741e01fb84
unknown <hyder1@gmail.com>
2022-12-02 20:23:56 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState([])
-  const baseUrl = "https://json-server-chi-nine.vercel.app:5000"
   // useEffect(() => {
   //   fetch('http://localhost:5000/data')
   //   .then(res => res.json())
@@ -12,24 +11,37 @@ function App() {
   // },[])
 
   function getDecks() {
-    fetch(baseUrl + '/decks')
+    fetch('http://localhost:5000/decks')
     .then(res => res.json())
     .then(data => setData(data))
   }
 
   function getCards() {
-    fetch(baseUrl + '/cards')
+    fetch('http://localhost:5000/cards')
     .then(res => res.json())
     .then(data => setData(data))
   }
 
+  function addDeck() {
+    fetch('http://localhost:5000/decks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({"name": "deck "+ Math.random() * 100 + 1})
+    })
+    .then(res => res.json())
+    .then(deck => setData([...data, deck]))
+  }
+// console.log(data)
   return (
     <div className="App">
       <button onClick={getDecks}>Get decks</button>
       <button onClick={getCards}>Get cards</button>
+      <button onClick={addDeck}>Add deck</button>
         {
-          data.map(item => (
-            <p key={item}>{item}</p>
+          data.map(({name, id}) => (
+            <p key={id}>{name}</p>
           ))
         }
     </div>
